d7fcd94de9f51c6eadcd17161f1306235c710eb5
Greg Stewart <gregs@tcias.co.uk>
2012-03-22 21:44:06 +0000

    Greg: fixed the URI for getting saved mood

--- public/javascripts/Mood.js
+++ public/javascripts/Mood.js
@@ -8,7 +8,7 @@
     Mood.prototype.getMoods = function(callback) {
       return $.ajax({
         type: "GET",
-        url: "http://localhost:3000/get/moods/",
+        url: "/get/moods/",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: callback,
